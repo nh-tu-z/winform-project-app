46377e188045ebef935465f9ddf5a077045cf41d
Ngo Tu <hoaitu1611@gmail.com>
2021-07-07 22:24:27 +0700

    backup thesis winform 070721

--- DesktopApp/Auth/LoginForm.Designer.cs
+++ DesktopApp/Auth/LoginForm.Designer.cs
@@ -32,13 +32,13 @@ namespace DesktopApp.Auth
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.lblErr = new System.Windows.Forms.Label();
-            this.txtPhone = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
             this.brnLogin = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -75,27 +75,6 @@ namespace DesktopApp.Auth
             this.panel2.Size = new System.Drawing.Size(290, 73);
             this.panel2.TabIndex = 2;
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.label4);
-            this.panel3.Controls.Add(this.label3);
-            this.panel3.Controls.Add(this.txtPassword);
-            this.panel3.Controls.Add(this.txtPhone);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 173);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(290, 123);
-            this.panel3.TabIndex = 3;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.brnLogin);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 296);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(290, 84);
-            this.panel4.TabIndex = 3;
-            // 
             // lblErr
             // 
             this.lblErr.AutoSize = true;
@@ -108,27 +87,17 @@ namespace DesktopApp.Auth
             this.lblErr.Text = "Err";
             this.lblErr.Visible = false;
             // 
-            // txtPhone
+            // panel3
             // 
-            this.txtPhone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(35)))), ((int)(((byte)(41)))));
-            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhone.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtPhone.Location = new System.Drawing.Point(15, 22);
-            this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(253, 26);
-            this.txtPhone.TabIndex = 0;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label3.Location = new System.Drawing.Point(12, 3);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 16);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Phone";
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtPassword);
+            this.panel3.Controls.Add(this.txtPhone);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 173);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(290, 123);
+            this.panel3.TabIndex = 3;
             // 
             // label4
             // 
@@ -141,6 +110,17 @@ namespace DesktopApp.Auth
             this.label4.TabIndex = 0;
             this.label4.Text = "Password";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.label3.Location = new System.Drawing.Point(12, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone";
+            // 
             // txtPassword
             // 
             this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(35)))), ((int)(((byte)(41)))));
@@ -152,6 +132,26 @@ namespace DesktopApp.Auth
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(253, 26);
             this.txtPassword.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(35)))), ((int)(((byte)(41)))));
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtPhone.Location = new System.Drawing.Point(15, 22);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(253, 26);
+            this.txtPhone.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.brnLogin);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 296);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(290, 84);
+            this.panel4.TabIndex = 3;
             // 
             // brnLogin
             // 
@@ -167,6 +167,7 @@ namespace DesktopApp.Auth
             this.brnLogin.TabStop = false;
             this.brnLogin.Text = "Login";
             this.brnLogin.UseVisualStyleBackColor = false;
+            this.brnLogin.Click += new System.EventHandler(this.brnLogin_Click);
             // 
             // btnLogin
             // 
--- DesktopApp/Auth/ProjectForm.cs
+++ DesktopApp/Auth/ProjectForm.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/* use to parse JSON */
+using Newtonsoft.Json.Linq;
 
 namespace DesktopApp.Auth
 {
     public partial class ProjectForm : Form
     {
-        public ProjectForm()
+        private int xCoordinate = 25;
+        private int yCoordinate = 130;
+
+        public ProjectForm(string tokenAuth, JObject resJson)
         {
             InitializeComponent();
+            /* convert JToken -> JArray */
+            var projIDArr = JArray.FromObject(resJson["devices"]);
+            int index = 1;
+            foreach (JObject item in projIDArr)
+            {
+                createAProItem(index.ToString(), xCoordinate, yCoordinate);
+                xCoordinate += 60;
+                createAProItem(item.GetValue("nameProject").ToString(), xCoordinate, yCoordinate);
+                xCoordinate += 240;
+                createAProItem(item.GetValue("addressProject").ToString(), xCoordinate, yCoordinate);
+                xCoordinate += 240;
+                createAProItem(checkState((bool)item.GetValue("activeProject")), xCoordinate, yCoordinate);
+                createABtn((bool)item.GetValue("activeProject"), xCoordinate + 90, yCoordinate - 7);
+                /* Reset Coordinate */
+                xCoordinate = 30;
+                yCoordinate += 40;
+            }
+        }
+
+        private void ProjectForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void createAProItem(string text, int x, int y)
+        {
+            Label lblProject = new Label()
+            {
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(46, 51, 73),
+                Font = new Font("Microsoft Sans Serif", 16, FontStyle.Bold),
+                Location = new Point(x, y),
+                Text = text,
+                AutoSize = true
+            };
+            this.Controls.Add(lblProject);
+        }
+        private void createABtn(bool state, int x, int y)
+        {
+            Button btnProject = new Button()
+            {
+                ForeColor = Color.FromArgb(46, 51, 73),
+                BackColor = Color.FromArgb(0, 126, 249),
+                Font = new Font("Microsoft Sans Serif", 15, FontStyle.Bold),
+                AutoSize = true,
+                Location = new Point(x, y),
+                Text = state ? "Monitoring" : "Back",
+                FlatStyle = FlatStyle.Flat
+            };
+            this.Controls.Add(btnProject);
+        }
+        private string checkState(bool state)
+        {
+            return state ? "Active" : "Unactive";
         }
     }
 }
